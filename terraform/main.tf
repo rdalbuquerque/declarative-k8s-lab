0c1ad1e97477301b02eed1aeaae17e7d2de6c548
rdalbuquerque <r.damiani@poli.ufrj.br>
2021-06-06 19:52:06 -0300

    fixing apiserver-advertise-address

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,46 +18,24 @@ provider "aws" {}
 # Setting up master node
 resource "aws_instance" "kube_master" {
   ami           = "ami-02e2a5679226e293c" # ID of default Debian 10 ami (64-bit|x86)
-  instance_type = "t2.micro" # minimum requirement for kubernetes is 2 cpus and 2 gb of ram wich would be a chargeable EC2, so we will try the micro anyways
-
-  tags = {
-    Name = "KubeMaster"
+  instance_type = "t2.micro" # To keep it free tier eligible we will be using t2.micro
+    Name = "kube-master"
   }
-
-  key_name = "rda"
+  key_name = "k8s-lab"
 
   provisioner "remote-exec" {
     connection {
       host = self.public_ip
       user = "admin"
-      private_key = file("~/.ssh/rda.pem")
+      private_key = file("~/.ssh/k8s-lab.pem")
     }
-
     inline = ["echo 'Instance ${self.public_dns} is up!'"]
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.public_ip},' --extra-vars 'private_ip=${self.private_ip} hostname=${split(".", self.private_dns)[0]}' --private-key ~/.ssh/rda.pem ../KubeSetup/master-playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.public_ip},' --extra-vars 'public_ip=${self.public_ip} hostname=${split(".", self.private_dns)[0]}' --private-key ~/.ssh/k8s-lab.pem ../ansible/master-playbook.yml"
   }
 }
-
-#resource "null_resource" "master_playbook" {
-#
-#  provisioner "remote-exec" {
-#    connection {
-#      host = aws_instance.kube_master.public_ip
-#      user = "admin"
-#      private_key = file("~/.ssh/rda.pem")
-#    }
-#
-#    inline = ["echo 'Instance ${aws_instance.kube_master.public_dns} is up!'"]
-#  }
-#
-#  provisioner "local-exec" {
-#    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${aws_instance.kube_master.public_ip},' --extra-vars 'private_ip=${aws_instance.kube_master.private_ip} hostname=${split(".", aws_instance.kube_master.private_dns)[0]}' --private-key ~/.ssh/rda.pem ../KubeSetup/master-playbook.yml"
-#  }
-#
-#}
 
 # Setting up worker nodes
 resource "aws_instance" "kube_worker" {
@@ -65,26 +43,22 @@ resource "aws_instance" "kube_worker" {
   count = 2
 
   ami           = "ami-02e2a5679226e293c" # ID of default Debian 10 ami (64-bit|x86)
-  instance_type = "t2.micro" # minimum requirement for kubernetes is 2 cpus and 2 gb of ram wich would be a chargeable EC2, so we will try the micro anyways
-
-  tags = {
-    Name = "KubeWorker"
+  instance_type = "t2.micro" # To keep it free tier eligible we will be using t2.micro
+    Name = "kube-worker"
   }
-
-  key_name = "rda"
+  key_name = "k8s-lab"
 
   provisioner "remote-exec" {
     connection {
       host = self.public_ip
       user = "admin"
-      private_key = file("~/.ssh/rda.pem")
+      private_key = file("~/.ssh/k8s-lab.pem")
     }
 
     inline = ["echo 'Instance ${self.public_dns} is up!'"]
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.public_ip},' --private-key ~/.ssh/rda.pem ../KubeSetup/node-playbook.yml"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -T 300 -i '${self.public_ip},' --private-key ~/.ssh/k8s-lab.pem ../ansible/node-playbook.yml"
   }
 }
-
